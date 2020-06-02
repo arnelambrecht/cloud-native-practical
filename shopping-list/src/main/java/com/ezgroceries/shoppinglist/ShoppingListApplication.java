37067c6532389c1482efc775e3504acef8a172b0
arnelambrecht <arne.lambrecht@kbc.be>
2020-06-02 10:42:18 +0200

    Update ShoppingListApplication.java

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShoppingListApplication {
 
 	public static void main(String[] args) {
+		// this is the starting point of the application
 		SpringApplication.run(ShoppingListApplication.class, args);
 	}
 
+
+	
 }
